b258e59b5fbbf5f94f4bf335da5a37d49318e614
Groodev <104547527+Groodev@users.noreply.github.com>
2025-06-22 16:12:43 +0700

    Create [BEGGINER] simpleInput program.py

---
+++ [BEGGINER] simpleInput program.py
@@ -0,0 +1,8 @@
+nama = input("nama: ")
+lagufav = input("favsong: ")
+usia = input("usia: ")
+hobi = input("hobi: ")
+
+mblibs = f"hai, {nama}! aku adalah komputer, program yang kamu buat. favsong kamu pasti {lagufav} ya? soalnya kamu orangnya edgy XD!. btw, di usia mu yang ke {usia}, kamu itu termasuk generasi emas loh! kembangkan hobi mu yaitu {hobi} agar kamu jadi orang sukses ya!..."
+
+print(mblibs)
